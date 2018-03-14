9a7a441319a5fb1fef611e70de111990e0bdefd9
Diego de Oliveira <dodiego@gmail.com>
2018-03-14 19:20:12 -0300

    improved parse performance of read statement result to json

--- mappers/cypher/read_statement_result_to_json.js
+++ mappers/cypher/read_statement_result_to_json.js
@@ -1,7 +1,9 @@
 const flatten = require('@flatten/array')
 
 function getSegments(result) {
+  // neo4j objects comes from the "_fields" property in each item of the "records" array
   let fields = flatten(result.records.map(s => s._fields))
+  // segments stores the relationship between objects in each row
   return flatten(fields.map(s => s.segments))
 
 }
@@ -9,11 +11,19 @@ function getSegments(result) {
 function segmentsToJson(segments) {
   let related = {}
   let nodes = segments.reduce((result, segment) => {
-    result[segment.start.properties.uuid] = result[segment.start.properties.uuid] || segment.start.properties
-    result[segment.end.properties.uuid] = result[segment.end.properties.uuid] || segment.end.properties
+    if (!result[segment.start.properties.uuid]) {
+      result[segment.start.properties.uuid] = segment.start.properties
+    }
+
+    if (!result[segment.end.properties.uuid]) {
+      result[segment.end.properties.uuid] = segment.end.properties
+    }
 
     if (segment.relationship.properties.isArray) {
-      result[segment.start.properties.uuid][segment.relationship.type] = result[segment.start.properties.uuid][segment.relationship.type] || []
+      if (!result[segment.start.properties.uuid][segment.relationship.type]) {
+        result[segment.start.properties.uuid][segment.relationship.type] = []
+      }
+
       result[segment.start.properties.uuid][segment.relationship.type].push(result[segment.end.properties.uuid])
     } else {
       result[segment.start.properties.uuid][segment.relationship.type] = result[segment.end.properties.uuid]
@@ -30,7 +40,6 @@ function readStamentResultToJson (result) {
 
   let segments = getSegments(result)
   let json = segmentsToJson(segments)
-
   return json
 }
 
