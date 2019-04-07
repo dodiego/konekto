e582ae5a497ae4fec03481bf014bf361f02c5c98
Diego de Oliveira <dodiego@gmail.com>
2019-04-07 13:21:13 -0300

    refactored save

--- test/manual.js
+++ test/manual.js
@@ -1,4 +1,5 @@
 const Aghanim = require('../lib')
+const { id, label } = require('../lib/utils')
 const aghanim = new Aghanim({
   user: 'agens',
   pass: 'agens',
@@ -6,6 +7,26 @@ const aghanim = new Aghanim({
 })
 
 async function run () {
+  let json = {
+    [label]: 'xd',
+    name: 'omegalul',
+    rel: {
+      [label]: 'xd2',
+      bool: true
+    }
+  }
   await aghanim.connect('agens_graph')
-  await aghanim.cre
+  await aghanim.createSchema(json)
+  await aghanim.save(json)
 }
+
+run().then(
+  () => {
+    console.log('success!')
+    process.exit(0)
+  },
+  err => {
+    console.error(err)
+    process.exit(1)
+  }
+)
