c2042f8db65ceb3d94201946185366f147839fae
Diego de Oliveira <dodiego@gmail.com>
2019-04-13 00:21:56 -0300

    added delete and fixed empty node read

--- test/manual.js
+++ test/manual.js
@@ -9,9 +9,7 @@ const aghanim = new Aghanim({
 async function run () {
   let json = {
     [label]: 'xd',
-    [where]: {
-      name: 'omegalul2'
-    },
+    name: 'omegalul2',
     rel: [
       {
         [label]: 'xd2',
@@ -27,38 +25,26 @@ async function run () {
       }
     ]
   }
+  let queryObject = {
+    [label]: 'xd',
+    name: 'omegalul2',
+    rel: [
+      {
+        [label]: 'xd2',
+        subRel: {
+          [label]: 'xd3'
+        }
+      }
+    ]
+  }
   await aghanim.connect('agens_graph')
   await aghanim.createSchema(json)
-  // // await aghanim.save(json)
-  // console.log(
-  //   JSON.stringify(
-  //     await aghanim.findByQueryObject({
-  //       [label]: 'xd',
-  //       name: 'omegalul2',
-  //       rel: [
-  //         {
-  //           [label]: 'xd2',
-  //           [where]: {
-  //             number: {
-  //               $gte: 10
-  //             }
-  //           },
-  //           subRel: {
-  //             [label]: 'xd3',
-  //             [where]: {
-  //               date: {
-  //                 $eq: '2019-04-13T01:21:14.291Z'
-  //               }
-  //             }
-  //           }
-  //         }
-  //       ]
-  //     }),
-  //     null,
-  //     2
-  //   )
-  // )
-  console.log(JSON.stringify(await aghanim.findById('5.1'), null, 2))
+  await aghanim.save(json)
+  let result = await aghanim.findByQueryObject(queryObject)
+  console.log(JSON.stringify(result, null, 2))
+  await aghanim.deleteById(result[0]._id)
+  // JSON.stringify(await aghanim.deleteByQueryObject(queryObject), null, 2)
+  // console.log(JSON.stringify(await aghanim.findById('5.1'), null, 2))
 }
 
 run().then(
