4323e1921dea926127ffee8c6ee13d3e85c1eece
Diego de Oliveira <dodiego@gmail.com>
2019-09-26 23:50:19 -0300

    - simplified beforeSave and beforeUpdate hooks - fixed read query when combining where with label filter

--- test/hooks.test.js
+++ test/hooks.test.js
@@ -9,22 +9,23 @@ describe('hooks', () => {
   })
 
   describe('beforeCreate', () => {
-    test('hide root node', async () => {
+    test('should throw error when hook returns false on root node', async () => {
       const json = {
         _label: 'test',
         omegalul: 'xd'
       }
       await konekto.createSchema(json)
-      const saveResult = await konekto.save(json, {
-        hooks: {
-          beforeCreate () {
-            return false
+      expect(
+        konekto.save(json, {
+          hooks: {
+            beforeCreate () {
+              return false
+            }
           }
-        }
-      })
-      expect(saveResult).toBeUndefined()
+        })
+      ).rejects.toThrowError()
     })
-    test('hide related node', async () => {
+    test('should throw error when hook returns false on related node', async () => {
       const json = {
         _label: 'test',
         omegalul: 'xd',
@@ -34,81 +35,15 @@ describe('hooks', () => {
         }
       }
       await konekto.createSchema(json)
-      const saveResult = await konekto.save(json, {
-        hooks: {
-          beforeCreate (node) {
-            if (node._label === 'test2') {
+      expect(
+        konekto.save(json, {
+          hooks: {
+            beforeCreate (node) {
               return false
             }
-            return true
           }
-        }
-      })
-      delete saveResult._id
-      delete json.related
-      expect(saveResult).toStrictEqual(json)
-    })
-    test('hide related graph', async () => {
-      const json = {
-        _label: 'test',
-        omegalul: 'xd',
-        related: {
-          _label: 'test2',
-          kek: 'w',
-          sub_related: {
-            _label: 'test3',
-            wat: 'js'
-          }
-        }
-      }
-      await konekto.createSchema(json)
-      const saveResult = await konekto.save(json, {
-        hooks: {
-          beforeCreate (node) {
-            if (node._label === 'test2') {
-              return false
-            }
-            return true
-          }
-        }
-      })
-      delete json.related
-      delete saveResult._id
-      expect(saveResult).toStrictEqual(json)
-    })
-
-    test('hide related sub graph', async () => {
-      const json = {
-        _label: 'test',
-        omegalul: 'xd',
-        related: {
-          _label: 'test2',
-          kek: 'w',
-          sub_related: {
-            _label: 'test3',
-            wat: 'js',
-            deeper_related: {
-              _label: 'test4',
-              sadim: true
-            }
-          }
-        }
-      }
-      await konekto.createSchema(json)
-      const saveResult = await konekto.save(json, {
-        hooks: {
-          beforeCreate (node) {
-            if (node._label === 'test3') {
-              return false
-            }
-            return true
-          }
-        }
-      })
-      delete saveResult._id
-      delete saveResult.related._id
-      delete json.related.sub_related
-      expect(saveResult).toStrictEqual(json)
+        })
+      ).rejects.toThrowError()
     })
   })
 
